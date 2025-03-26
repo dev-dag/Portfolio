027723526d6d67a362c3c1a59e162db0534c3f02
dag <rubtub2314@gmail.com>
2025-03-26 10:31:48 +0900

    다이얼로그 래퍼 클래스 추가

--- Assets/2.Script/Database Tables/Dialog.cs
+++ Assets/2.Script/Database Tables/Dialog.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using SQLite4Unity3d;
+using System.Collections.Generic;
 
 namespace Database_Table
 {
@@ -14,5 +15,19 @@ namespace Database_Table
 
         [Column("dialog_text")]
         public string DialogText { get; set; }
+    }
+
+    public class DialogWrapper
+    {
+        public int ID { get; private set; }
+        public Dictionary<int, string> DialogText { get; private set; } // Dictionary<Index, DialogText>
+
+        private DialogWrapper() {}
+
+        public DialogWrapper(int ID)
+        {
+            this.ID = ID;
+            DialogText = new Dictionary<int, string>();
+        }
     }
 }
