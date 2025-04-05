d80d0bd32c1a34b7aed93dd463fa55fce3e1d383
dag <rubtub2314@gmail.com>
2025-04-05 21:21:40 +0900

    보스 이미지 크기 조정

--- Assets/2.Script/Core/Combat System/SkillData.cs
+++ Assets/2.Script/Core/Combat System/SkillData.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Skill Data", menuName = "Scriptable Object/Skill Data")]
 public class SkillData : ScriptableObject
 {
+    public enum SkillType
+    {
+        Normal = 0,
+        Parryable = 1, // 패리 가능한 스킬
+        Parried = 2, // 패리 대상 스킬
+    }
+
     public enum SkillCollisionType
     {
         Box = 0,
@@ -10,6 +17,7 @@ public class SkillData : ScriptableObject
     }
 
     public SkillCollisionType collisionType;
+    public SkillType skillType;
     public float damage;
     [Tooltip("when collision type is box only.")] public Vector2 size;
     [Tooltip("when collision type is circle only.")] public float radius;
