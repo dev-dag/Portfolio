7a34be99bf0a41e6d060dd38dc221e0bf97a9f69
dag <rubtub2314@gmail.com>
2025-05-09 05:33:58 +0900

    사운드 나는 버튼 추가

---
+++ Assets/3.UI/Sound Button/SoundButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// UI 버튼에 소리 효과를 추가하는 컴포넌트.
+/// </summary>
+[RequireComponent(typeof(Button))]
+public class SoundButton : BaseObject, IPointerClickHandler, IPointerEnterHandler
+{
+    [SerializeField] private AudioClip overAudioClip;
+    [SerializeField] private AudioClip clickAudioClip;
+
+    // 클릭 되었을 때 SFX 재생
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (GameManager.Instance.audioSystem.IsInit)
+        {
+            GameManager.Instance.audioSystem.PlaySFX(AudioSystem.AudioType.UI_SFX, clickAudioClip);
+        }
+    }
+
+    // 오버 되었을 때 SFX 재생
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (GameManager.Instance.audioSystem.IsInit)
+        {
+            GameManager.Instance.audioSystem.PlaySFX(AudioSystem.AudioType.UI_SFX, overAudioClip);
+        }
+    }
+}
